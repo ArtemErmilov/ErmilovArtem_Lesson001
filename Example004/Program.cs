dc111bda530ab4c03422564be94d766adca1f31c
Artem Ermilov <ermilo-artjom@rambler.ru>
2022-06-09 19:18:55 +0300

    Задача 4

---
+++ Example004/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 4: Напишите программу, которая на вход
+// принимает число (N), а на выходе показывает все чётные
+// числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите число ->");
+string aString = Console.ReadLine();
+int a = int.Parse(aString);
+int contr = 1;
+
+while (contr<=a)
+{
+    if (contr%2 == 0)
+    {
+        Console.Write($"{contr}");
+        if ((a%2==0 & contr==a)||(a%2==1 & contr==a-1))
+        { 
+            Console.WriteLine(". "); 
+        }
+        else
+        {
+           Console.Write(", "); 
+        }
+    }
+    
+
+contr++;
+}
+
+
